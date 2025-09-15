e0220cf44aeb6fb750438fb80af65dc9cec06c9c
Jason Nathannael <Jasonnathannael05@gmail.com>
2025-09-15 09:55:28 +0700

    Membuat Inventory App yang menghubungkan product model dan product table model

--- src/untar/java/tugas1/ProductTableModel.java
+++ src/untar/java/tugas1/ProductTableModel.java
@@ -7,53 +7,53 @@ public class ProductTableModel extends AbstractTableModel {
     private final String[] columnNames = {"Code", "Name", "Quantity", "Price"};
     private ArrayList<ProductModel> products;
 
-    public ProductTableModel() {
+    public ProductTableModel () {
         this.products = new ArrayList<>();
     }
 
-    public void addProduct(ProductModel product) {
-        products.add(product);
-        fireTableRowsInserted(products.size() - 1, products.size() - 1);
+    public void addProduct (ProductModel product) {
+        products.add (product);
+        fireTableRowsInserted (products.size () - 1, products.size () - 1);
     }
 
-    public void removeProduct(int index) {
-        if (index >= 0 && index < products.size()) {
-            products.remove(index);
-            fireTableRowsDeleted(index, index);
+    public void removeProduct (int index) {
+        if (index >= 0 && index < products.size ()) {
+            products.remove (index);
+            fireTableRowsDeleted (index, index);
         }
     }
 
     public ProductModel getProduct(int index) {
-        if (index >= 0 && index < products.size()) {
-            return products.get(index);
+        if (index >= 0 && index < products.size ()) {
+            return products.get (index);
         }
         return null;
     }
 
     @Override
-    public int getRowCount() {
-        return products.size();
+    public int getRowCount () {
+        return products.size ();
     }
 
     @Override
-    public int getColumnCount() {
+    public int getColumnCount () {
         return columnNames.length;
     }
 
     @Override
-    public Object getValueAt(int rowIndex, int columnIndex) {
-        ProductModel product = products.get(rowIndex);
+    public Object getValueAt (int rowIndex, int columnIndex) {
+        ProductModel product = products.get (rowIndex);
         switch (columnIndex) {
-            case 0: return product.getCode();
-            case 1: return product.getName();
-            case 2: return product.getQty();
-            case 3: return product.getPrice();
+            case 0: return product.getCode ();
+            case 1: return product.getName ();
+            case 2: return product.getQty ();
+            case 3: return product.getPrice ();
             default: return null;
         }
     }
 
     @Override
-    public String getColumnName(int column) {
+    public String getColumnName (int column) {
         return columnNames[column];
     }
 }
